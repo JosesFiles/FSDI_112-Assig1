35fe86edf8c1b529d8c2c1ce5cb4d74898506b97
JoseFiles <jose.cisneros1981@gmail.com>
2023-02-15 20:26:03 -0800

    FSDI 112 final

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomepageView, AboutPageView
+from .views import HomePageView, AboutPageView
 
 
 urlpatterns = [
-    path("", AboutPageView.as_view(),name="home"),
+    path("", HomePageView.as_view(),name="home"),
     path("about/", AboutPageView.as_view(),name="about"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,9 @@
 from django.views.generic import TemplateView
 
 
-class HomepageView(TemplateView):
+class HomePageView(TemplateView):
     template_name = "home.html"
 
-    class AboutPageview(TemplateView):
-        template_name = "about.html"
+class AboutPageView(TemplateView):
+    template_name = "about.html"
         
